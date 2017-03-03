905072960798fc76bc3ec0e589cb5ddb6eef7a4a
Kae Anderson <andersonkaem@gmail.com>
2017-03-02 18:02:26 -0600

    started HTML

---
+++ app/components/application.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="app">
+    <div class="nav">Marvel</div>
+      <div class="left-side">
+        <img src="http://www.cutestpaw.com/wp-content/uploads/2011/11/Lemon.jpg" alt="">.left-pic
+      </div> <!--end of left-side-->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
 import 'whatwg-fetch';
+
+// import Vue from 'vue';
+import app from './components/application.vue';
+
+// const app = new Vue(App);
+
+app.$mount('.app');
